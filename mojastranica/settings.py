6405015a11a75ffda8244ee0bbf04c41236bd92a
eminam98 <72666964+eminam98@users.noreply.github.com>
2020-10-11 19:22:59 +0200

    Update settings.py

--- mojastranica/settings.py
+++ mojastranica/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,"static")]
-PROJECT_ROOT = os.path.dirname(os.path.abspath(file))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(open))
 STATIC_ROOT=os.path.join(PROJECT_ROOT, 'static')
 
 
