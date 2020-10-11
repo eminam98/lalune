bfde5c86edce7734384456a2c6682586b90b000e
eminam98 <72666964+eminam98@users.noreply.github.com>
2020-10-11 20:14:23 +0200

    Update urls.py

--- mojastranica/urls.py
+++ mojastranica/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('lalune/', include('LaLune.urls')),
+    path('', include('LaLune.urls')),
 
     url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
